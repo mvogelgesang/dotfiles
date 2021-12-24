b3c189bd26f4a91fb4797f381f3dc8e2f0c8d549
mvogelgesang <movogelgesang@gmail.com>
2021-12-23 22:18:22 -0500

    added context to install.sh, added ruby and zsh

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,7 +17,7 @@ function install {
   fi
 }
 
-# Basics
+# Basics - calls the function above which checks if the programs are installd or not
 install awscli
 install curl
 install exfat-utils
@@ -27,9 +27,18 @@ install nmap
 install tree
 install vim
 install wget
+install libnss3-tools
+
 
 # Run all scripts in programs/
 for f in programs/*.sh; do bash "$f" -H; done
+
+# Setup localhost https cert
+wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
+sudo mv mkcert-v1.4.3-linux-amd64 /usr/bin/mkcert
+sudo chmod +x /usr/bin/mkcert
+mkcert -install
+mkcert localhost
 
 # Get all upgrades
 sudo apt upgrade -y
--- scripts/programs/zsh.sh
+++ scripts/programs/zsh.sh
@@ -2,6 +2,9 @@
 
 echo "📦 Installing zsh"
 
+sudo apt install -y zsh
+zsh --version
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 chsh -s `which zsh`
