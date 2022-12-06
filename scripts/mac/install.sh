f1a13cf8a5e1e11a4c9fae2be39d2d307ddbacb7
mvogelgesang <movogelgesang@gmail.com>
2022-12-06 08:31:23 -0500

    Cleaned up file, added some additional React Native bits, set headers to print.

--- scripts/mac/install.sh
+++ scripts/mac/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew update
-
 function brewInstall {
   which $1 &> /dev/null
 
@@ -16,14 +11,35 @@ function brewInstall {
   fi
 }
 
-brewInstall visual-studio-code --cask
-brewInstall docker --cask
-brewInstall rectangle --cask
-brewInstall discord --cask
-brewInstall vlc --cask
-brewInstall android-studio --cask
-brewInstall nvm
+function printSection {
+  which $1 &> /dev/null
 
+  if [ $? -ne 0 ]; then
+    echo "----------------"
+    echo ">>> ${1}"
+    echo "----------------"
+  else
+
+  fi
+}
+
+# Homebrew
+printSection Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+
+brewInstall android-studio --cask
+brewInstall discord --cask
+brewInstall docker --cask
+brewInstall nvm
+brewInstall rectangle --caskbr
+brewInstall visual-studio-code --cask
+brewInstall vlc --cask
+brewInstall watchman
+
+# ohmyzsh
+printSection ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 omz update
@@ -32,9 +48,9 @@ source ~/.zshrc
 brew install starship
 
 ## SSH
+printSection ssh
 mkdir ~/.ssh
 cd ~/.ssh
-
 ssh-keygen -t ed25519 
 # Confirm whether passphrase was used properly by accessing private key:
 ssh-keygen -y -f id_ed25519
@@ -44,36 +60,31 @@ echo "Host *
   AddKeysToAgent yes
   UseKeychain yes
   IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
-
 ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 
 # add the public key to github via the GH cli
 # copy public key and add it to https://github.com/
 cat ~/.ssh/id_ed25519.pub | pbcopy
 
-gh ssh-key add ~/.ssh/id_ed25519.pub -t github
-
-source ~/.zshrc
-
+# node, nvm, npm
+printSection "Node & NPM"
 nvm install --lts
-
 node -v && npm -v
-
 npm install -g npm@latest
 
-# needs work
-# npm config set --init-author-name "Mark Vogelgesang"
-# npm config set --init-author-email "movogelgesang@gmail.com"
-# npm config set --init-author-url "htttps://mvogelgesang.com"
-# npm add user
+# global node packages
+printSection "Node Packages -g"
+./node.sh
 
-npm install -g yarn
-yarn -v
+# reactNative
+printSection "React Native"
+./reactNative.sh
+
 # Other System Preferences
-
+printSection "Other System Preferences"
 # show hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
 
 # Cross-environment config
-
-bash ../shared.sh
+printSection "Cross-environment Config"
+bash ./shared.sh
